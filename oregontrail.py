ef40d4be9a7db78d004cc8cc668c3702cfb15c90
Brenden Smit <brenden.smit@mnsu.edu>
2025-10-28 10:40:18 -0500

    passengers

--- oregontrail.py
+++ oregontrail.py
@@ -1,1 +1,6 @@
 #Here is our code for amazing Oregon Trail Game
+
+driver = input("Enter first name of wagon leader: ")
+passenger1 = input("Enter first name of first passenger: ")
+passenger2 = input("Enter first name of second passenger: ")
+passenger3 = input("Enter first name of third passenger: ")
