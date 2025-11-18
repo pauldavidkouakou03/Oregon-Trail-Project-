6b0d369ff122bdeddd076dfe2140b90dd9ab1aec
Brenden Smit <brenden.smit@mnsu.edu>
2025-11-18 09:38:32 -0600

    Update paul.py

--- paul.py
+++ paul.py
@@ -1,16 +1,3 @@
-def get_health(self):
-def get_fuel(self):
-def get_hunger(self):
-def get_phone_battery(self):
-def get_snacks(self):
-    
-def end_game_screen(stats):
-    print("Game Over!")
-    print("Here are your stats:")
-    for stat, value in stats.items():
-        print(f"{stat}: {value}")
-    print("Thanks for playing!")
-
 
 
 
