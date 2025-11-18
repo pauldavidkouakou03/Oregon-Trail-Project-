8855ab4e89cd00600131d5a93671f68da88a0e57
Brenden Smit <brenden.smit@mnsu.edu>
2025-11-18 09:37:01 -0600

    Update paul.py

--- paul.py
+++ paul.py
@@ -1,10 +1,3 @@
-def get_health(self):
-def get_fuel(self):
-def get_hunger(self):
-def get_phone_battery(self):
-def get_snacks(self):
-
-
 #Create a project Oregan Trail game 
 class Player:
     def __init__(self, name, health=100, supplies=50):
