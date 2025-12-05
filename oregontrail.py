26b1c4cd38f106be3c329322607d648cd869b6af
Aidan-Tesch <bt4538lm@go.minnstate.edu>
2025-12-05 01:44:04 -0600

    Fixed a bug with one person events not pulling the sample correctly

--- oregontrail.py
+++ oregontrail.py
@@ -509,11 +509,12 @@ class Events:
 
     def one_person_useless_events():
         living = get_living_passengers()
-        passenger = random.sample(living)
+        passenger = random.sample(living, 1)
         one_person_events = (
             f"{passenger[0].get_name()} is wondering why ranch dressing isn't its own food group...",
             f"{passenger[0].get_name()} is wondering if a straw has only one hole or two...",
-            f"{passenger[0].get_name()} is contemplating about if water is actually wet...",       
+            f"{passenger[0].get_name()} is contemplating about if water is actually wet...",
+            f"{passenger[0].get_name()} farted...",              
         )
         print(random.choice(one_person_events))
 
